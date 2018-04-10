0a0afd4cd6faab868c8d15189d9b25f41f478c4c
bomee888 <bomee888@naver.com>
2018-04-10 18:05:58 +0900

    Site updated: 2018-04-10 18:05:56

--- js/customizing.js
+++ js/customizing.js
@@ -1,3 +1,71 @@
+
 $(window).load(function(){
-  alert("ok load!!");
+	$(".menu-icon").children("button").click(function(){
+		$(".left-side").addClass("on");
+	});
+	$(".left-side").children(".close").click(function(){
+		$(".left-side").removeClass("on");
+	});
+});
+
+
+// $(window).scroll(function(){
+// 	var scrTop = $(window).scrollTop();
+// 	if(scrTop++){
+// 		$(".custom-nav").fadeOut(300);
+// 	}else if(scrTop--){
+// 		$(".custom-nav").fadeIn(300);
+// 	}
+// });
+
+
+
+$(document).ready(function() {
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.custom-nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }﻿
+}, 0);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    console.log(st);
+    if(st <= 0){
+    	$(".custom-nav").addClass("top");
+    }else{
+    	$(".custom-nav").removeClass("top");
+    }
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $(".custom-nav").fadeOut(300);
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $(".custom-nav").fadeIn(300);
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
 });
